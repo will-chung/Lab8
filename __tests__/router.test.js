5a6fa1e907dde3070273f0a7e578eb156ea14b8f
will-chung <billchung6@gmail.com>
2021-05-23 18:08:44 -0700

    Update router.test.js

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,21 @@
  * @jest-environment jsdom
  */
 
+describe('pushToHistory', () => {
+    test('increments history stack length', () => {
+        const preLength = history.length;
+        expect(pushToHistory('home').length).toBe(preLength+1);
+    });
+
+    test('pushes correct state for homepage', () => {
+        expect(pushToHistory('home').state).toEqual({});
+    });
+
+    test('pushes correct state for settings page', () => {
+        expect(pushToHistory('settings').state).toEqual({ page: 'settings' });
+    });
+
+    test('pushes correct state for entry page', () => {
+        expect(pushToHistory('entry', 1).state).toEqual({ page: 'entry1' });
+    });
+});
